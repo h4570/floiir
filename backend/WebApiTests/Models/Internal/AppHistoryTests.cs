4cc314c54cb5c69a04bf741869b1e0c7f5891558
Sandro Sobczyński <hastoklai@gmail.com>
2020-08-08 17:43:59 +0200

    changed xunit to msbuild

--- backend/WebApiTests/Models/Internal/AppHistoryTests.cs
+++ backend/WebApiTests/Models/Internal/AppHistoryTests.cs
@@ -1,17 +1,22 @@
-﻿using Xunit;
-using WebApi.Models.Internal;
+﻿using WebApi.Models.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WebApi.Models.Internal.Tests
 {
+    [TestClass()]
     public class AppHistoryTests
     {
-        [Fact()]
-        public void AppHistoryTest()
+
+        [TestMethod()]
+        public void AppHistoryTest1()
         {
-            Assert.True(true, "Example unit test");
+            var desc = "X5cR3OY6AOPAIgxnRq1EeaAliIIs2giXkCJFKLBuPZI6CBSwKNLLKpePACWBd0efXnEnnpzCuBvuKg7EYpuvvHxRWnqTlDsIujAsdbFa5Dj1f6nZgObEc4ujP5Os6mCowJGjzuX3aN43bK5P31Wy9C18TsXPKOWXdB5KQuWYOsWZRsm9pujlikzHJA2ihvAJlQCtCqcatMF27GJBOP";
+            var obj = new AppHistory(AppTable.TableName, AppHistoryType.Add, 1, "1", 1, desc);
+            Assert.IsTrue(obj.Description.Length == 199);
         }
+
     }
 }
