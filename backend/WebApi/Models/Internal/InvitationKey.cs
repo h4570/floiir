d74f68767a6337c87608f9de68cf19cdc61814bb
Sandro Sobczyński <SANDRO_SOBCZYNSKI@jabil.com>
2020-08-18 12:14:02 +0200

    created prototype models

---
+++ backend/WebApi/Models/Internal/InvitationKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Internal
+{
+
+    public class InvitationKey
+    {
+        public int Id { get; set; }
+        [StringLength(10)]
+        public string Key { get; set; }
+        public int InviterId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? UsedByUserId { get; set; }
+    }
+}
---
+++ backend/WebApi/Models/Internal/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Internal
+{
+
+    public class User
+    {
+        public int Id { get; set; }
+        [StringLength(35)]
+        public string FirstName { get; set; }
+        [StringLength(35)]
+        public string LastName { get; set; }
+        [StringLength(254)]
+        public string Email { get; set; }
+    }
+}
