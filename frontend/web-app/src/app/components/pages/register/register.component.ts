7b6ccc93088d48c726a3e299e386472491d287cd
Sandro Sobczyński <SANDRO_SOBCZYNSKI@jabil.com>
2020-08-18 14:00:08 +0200

    added invitation-key service (to use get endpoint)

--- frontend/web-app/src/app/components/pages/register/register.component.ts
+++ frontend/web-app/src/app/components/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { InvitationKeyService } from 'src/app/services/invitation-key.service';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private invitationKeyService: InvitationKeyService
+  ) { }
 
-  ngOnInit(): void {
+  public async ngOnInit(): Promise<any> {
     const key = this.route.snapshot.paramMap.get('key');
-    console.log(key);
+    // if (key.length === 10) { // TODO: extension method
+    const response = await this.invitationKeyService.get(key);
+    console.log(response);
+    // }
   }
 
 }
--- frontend/web-app/src/app/components/pages/register/register.module.ts
+++ frontend/web-app/src/app/components/pages/register/register.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
+import { InvitationKeyService } from 'src/app/services/invitation-key.service';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,9 @@ import { RegisterComponent } from './register.component';
   ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    InvitationKeyService
   ]
 })
 export class RegisterModule { }
