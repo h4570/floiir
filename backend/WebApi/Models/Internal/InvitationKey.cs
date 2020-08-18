5720bd417c7ae8310c14ee8aa1596412df694ce7
Sandro Sobczyński <SANDRO_SOBCZYNSKI@jabil.com>
2020-08-18 12:21:01 +0200

    added foreign keys reference

--- backend/WebApi/Models/Internal/InvitationKey.cs
+++ backend/WebApi/Models/Internal/InvitationKey.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApi.Models.Internal
 {
@@ -9,8 +10,12 @@ namespace WebApi.Models.Internal
         public int Id { get; set; }
         [StringLength(10)]
         public string Key { get; set; }
+        [ForeignKey("User")]
         public int InviterId { get; set; }
+        public User Inviter { get; set; }
         public DateTime CreatedAt { get; set; }
+        [ForeignKey("User")]
         public int? UsedByUserId { get; set; }
+        public User UsedByUser { get; set; }
     }
 }
