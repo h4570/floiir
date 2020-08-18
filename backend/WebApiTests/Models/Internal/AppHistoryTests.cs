0eeae8daef68b3c7d0dbc643dbaf606577a6b743
Sandro Sobczyński <SANDRO_SOBCZYNSKI@jabil.com>
2020-08-18 12:30:58 +0200

    removed old property filling from mock function

--- backend/WebApiTests/Models/Internal/AppHistoryTests.cs
+++ backend/WebApiTests/Models/Internal/AppHistoryTests.cs
@@ -16,7 +16,7 @@ namespace WebApi.Models.Internal.Tests
             var desc = new StringBuilder();
             for (var i = 0; i < 250; i++)
                 desc.Append(i);
-            var obj = new AppHistory(AppTable.TableName, AppHistoryType.Add, 1, "1", 1, desc.ToString());
+            var obj = new AppHistory(AppTable.TableName, AppHistoryType.Add, 1, 1, desc.ToString());
             Assert.IsTrue(obj.Description.Length == 199);
         }
 
