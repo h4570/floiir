fba9a4a16d21c0b2cc3c0da46980bc1d279079cd
Sandro Sobczyński <SANDRO_SOBCZYNSKI@jabil.com>
2020-08-18 13:11:18 +0200

    added invitation key validation (length of key)

---
+++ backend/WebApi/Extensions/InvitationKey/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Extensions.InvitationKey
+{
+    public static class StringExtensions
+    {
+
+        /// <summary>
+        /// Checks if invitation key length is valid
+        /// </summary>
+        public static bool IsValid(this string invitationKey)
+        {
+            return invitationKey.Length == 10;
+        }
+    }
+}
