50935cabde12112c605b98b84e0f7c6db8aaec21
Sandro Sobczyński <hastoklai@gmail.com>
2020-08-20 21:53:54 +0200

    added login and pass to user model

--- backend/WebApi/Models/Internal/User.cs
+++ backend/WebApi/Models/Internal/User.cs
@@ -5,7 +5,14 @@ namespace WebApi.Models.Internal
 
     public class User
     {
+
         public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Login { get; set; }
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
         [Required]
         [StringLength(35)]
         public string FirstName { get; set; }
--- frontend/web-app/src/app/models/user.model.ts
+++ frontend/web-app/src/app/models/user.model.ts
@@ -3,6 +3,12 @@ export interface User {
     /** Primary key */
     id: number;
 
+    /** Max length: 20, non-nullable */
+    login: string;
+
+    /** Max length: 254, non-nullable */
+    password: string;
+
     /** Max length: 35, non-nullable */
     firstName: string;
 
