36b3a45236d7fb9bc053ca131c2ab930dbf786e2
Sandro Sobczyński <hastoklai@gmail.com>
2020-08-20 22:01:15 +0200

    registration form

--- frontend/web-app/src/app/components/pages/register/register.component.ts
+++ frontend/web-app/src/app/components/pages/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InvitationKeyService } from 'src/app/services/invitation-key.service';
 import { InvitationKeyManager } from './invitation-key-manager';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,8 +13,9 @@ import { InvitationKeyManager } from './invitation-key-manager';
 export class RegisterComponent implements OnInit {
 
   constructor(
-    private route: ActivatedRoute,
-    private invKeyService: InvitationKeyService
+    private readonly route: ActivatedRoute,
+    private readonly http: HttpClient,
+    private readonly invKeyService: InvitationKeyService
   ) {
     this.invKeyManager = new InvitationKeyManager(this.invKeyService);
   }
@@ -20,9 +23,35 @@ export class RegisterComponent implements OnInit {
   // Managers
   public invKeyManager: InvitationKeyManager;
 
+  // Data
+  public terms: string;
+
+  // Form
+  public registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    login: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    email: new FormControl(''),
+  });
+
   public async ngOnInit(): Promise<void> {
+    this.terms = await this.getAppTerms();
     const key = this.route.snapshot.paramMap.get('key');
     await this.invKeyManager.init(key);
+  }
+
+  public async onSubmit(): Promise<void> {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.registerForm.value);
+  }
+
+  private async getAppTerms(): Promise<string> {
+    return this.http
+      .get<string>('assets/terms.txt', { responseType: 'text' as 'json' })
+      .toPromise()
+      .then(result => result as any as string);
   }
 
 }
--- frontend/web-app/src/app/components/pages/register/register.module.ts
+++ frontend/web-app/src/app/components/pages/register/register.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { InvitationKeyService } from 'src/app/services/invitation-key.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [
     InvitationKeyService
