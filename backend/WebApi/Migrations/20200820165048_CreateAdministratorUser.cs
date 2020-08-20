0c80326e8d515c207df9f30663e8f6a9f8653034
Sandro Sobczyński <hastoklai@gmail.com>
2020-08-20 19:59:30 +0200

    sql migrations for admin user and first inv key

---
+++ backend/WebApi/Migrations/20200820165048_CreateAdministratorUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class CreateAdministratorUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+                .Sql("INSERT INTO public.\"Users\"(\"FirstName\", \"LastName\", \"Email\") " +
+                "VALUES('Administrator', 'Floiir', 'administrator@floiir.com');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+                .Sql("DELETE FROM public.\"Users\" WHERE \"FirstName\" = 'Administrator' AND \"LastName\" = 'Floiir';");
+        }
+    }
+}
