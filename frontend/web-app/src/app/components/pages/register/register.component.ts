5448f1c17defb06569ac862ed9eb649449f9798c
Sandro Sobczyński <SANDRO_SOBCZYNSKI@jabil.com>
2020-08-21 15:39:30 +0200

    refactored in key manager to service

--- frontend/web-app/src/app/components/pages/register/register.component.ts
+++ frontend/web-app/src/app/components/pages/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { InvitationKeyService } from 'src/app/services/invitation-key.service';
-import { InvitationKeyManager } from './invitation-key-manager';
 
 @Component({
   selector: 'app-register',
@@ -13,16 +12,11 @@ export class RegisterComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private invKeyService: InvitationKeyService
-  ) {
-    this.invKeyManager = new InvitationKeyManager(this.invKeyService);
-  }
-
-  // Managers
-  public invKeyManager: InvitationKeyManager;
+  ) { }
 
   public async ngOnInit(): Promise<void> {
     const key = this.route.snapshot.paramMap.get('key');
-    await this.invKeyManager.init(key);
+    await this.invKeyService.init(key);
   }
 
 }
