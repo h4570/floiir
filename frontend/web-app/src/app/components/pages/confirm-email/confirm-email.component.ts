19aaed4a486c48f5f3e578fe3cfed96c7ab98a2a
Sandro Sobczyński <hastoklai@gmail.com>
2020-09-01 14:03:19 +0200

    added confirm email component

--- frontend/web-app/src/app/components/pages/confirm-email/confirm-email.component.ts
+++ frontend/web-app/src/app/components/pages/confirm-email/confirm-email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-confirm-email',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmEmailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly snackBar: MatSnackBar,
+  ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
   }
+
+  public async onResendEmailClick(): Promise<void> {
+    const text = 'Email was sent again!'; // this.translate.instant('confirmEmail.emailResent') as string;
+    this.snackBar.open(text, 'OK', { duration: 5000 });
+  }
+
 
 }
--- frontend/web-app/src/app/components/pages/confirm-email/confirm-email.module.ts
+++ frontend/web-app/src/app/components/pages/confirm-email/confirm-email.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmEmailComponent } from './confirm-email.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     ConfirmEmailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatSnackBarModule,
   ]
 })
 export class ConfirmEmailModule { }
