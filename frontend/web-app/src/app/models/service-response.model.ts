4a54b2bbc5dad323fd1556e969ad507bbbc67d7f
Sandro Sobczyński <hastoklai@gmail.com>
2020-09-03 18:43:53 +0200

    refactored service-response class

--- frontend/web-app/src/app/models/service-response.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ServiceResponse {
-
-    /* data provided by http request */
-    data: any;
-
-    /** response code enum  */
-    responseCode: any;
-}
-
--- frontend/web-app/src/app/services/map.service.ts
+++ frontend/web-app/src/app/services/map.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { MapHttpService } from './http/map.http.service';
-import { ServiceResponse } from '../models/service-response.model';
+import { ServiceResponse } from '../dtos/service-response.dto';
+import { TileLayer } from 'leaflet';
 
 export enum MapGetResponse {
     Success,
@@ -12,21 +13,19 @@ export enum MapGetResponse {
 @Injectable()
 export class MapService extends MapHttpService {
 
-
-
     public error: any;
 
     private keyCheckResponse: MapGetResponse;
 
     /** Get tiles to build map from api base on x,y,z. */
-    public async getTiles(): Promise<ServiceResponse> {
-        return new Promise<ServiceResponse>(async (res) => {
-            const response: ServiceResponse = {
+    public async getTiles(): Promise<ServiceResponse<TileLayer, MapGetResponse>> {
+        return new Promise<ServiceResponse<TileLayer, MapGetResponse>>(async (res) => {
+            const response: ServiceResponse<TileLayer, MapGetResponse> = {
                 responseCode: undefined,
                 data: undefined
             };
             try {
-                response.data = await this.mapTiles();
+                response.data = this.mapTiles();
                 response.responseCode = MapGetResponse.Success;
             } catch (err) {
                 this.error = err;
