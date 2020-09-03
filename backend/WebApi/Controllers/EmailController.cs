003c3c455f9c262708614c4e4d6a2ac1baacb477
Sandro Sobczyński <hastoklai@gmail.com>
2020-09-03 18:36:35 +0200

    created email test endpoint

---
+++ backend/WebApi/Controllers/EmailController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebApi.Builders;
+using WebApi.Extensions;
+using WebApi.Misc.Auth;
+using WebApi.Models.Internal;
+
+namespace WebApi.Controllers
+{
+
+    [EnableCors]
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+
+        /// <summary>
+        /// TODO. Prototype of login endpoint
+        /// </summary>
+        /// <param name="payload"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize]
+        public ContentResult Test()
+        {
+            var builder = new BlueGrayEmailBuilder();
+            var director = new EmailDirector(builder);
+            // TODO i18n
+            var emailHtml = director.GetConfirmEmailEmailHtml(HttpContext.GetUserLanguage(), HttpContext.GetUser());
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = emailHtml
+            };
+        }
+
+    }
+}
